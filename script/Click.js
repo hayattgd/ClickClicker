4a6f167d2edf18b2573da5590a61bad53a2d8a7d
hayattgd <hayattgd@gmail.com>
2022-02-13 23:02:08 +0900

    Fixed up some little bugs

--- script/Click.js
+++ script/Click.js
@@ -3,5 +3,5 @@ let counter = document.getElementsByClassName("Counter")
 
 function onClick() {
   point += 1
-  counter[0].innerHTML = "POINTS:" + point.toString()
+  counter[0].innerHTML = point.toString()
 }
