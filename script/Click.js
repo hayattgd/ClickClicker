5b28affc6db1808fa95583062933e43a7ebea141
hayattgd <hayattgd@gmail.com>
2022-02-14 01:08:51 +0900

    added Upgrade

--- script/Click.js
+++ script/Click.js
@@ -1,7 +1,15 @@
 let point = 0
 let counter = document.getElementsByClassName("Counter")
+let upgrades = new Array(1)
+
+//Init
+upgrades[0] = 1
 
 function onClick() {
-  point += 1
+  point += upgrades[0]
   counter[0].innerHTML = point.toString()
+}
+
+function Upgrade1() {
+  upgrades[0] += 1
 }
