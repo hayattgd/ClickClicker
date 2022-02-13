8ddece21efe06b7f557c3455a0dd3ff0509a660e
hayattgd <hayattgd@gmail.com>
2022-02-13 15:18:06 +0900

    Update DetectLocal.js its successfully work!

--- script/DetectLocal.js
+++ script/DetectLocal.js
@@ -1,3 +1,12 @@
 let title = document.getElementById("web_title")
+let isLocal = false
 
-alert(title.value)
+if (location.protocol == "file:")
+{
+    isLocal = true
+    console.log("now Local mode enabled")
+    title.innerHTML = title.innerHTML + " - Local"
+}else
+{
+    console.log("now Local mode disabled")
+}
